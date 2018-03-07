80b47ff951ccb0f015408fbc063eb3fedca33f2f
ced <zuger.cedric@gmail.com>
2018-03-07 11:49:47 +0100

    Threads reviewed.

--- server/folders.rb
+++ server/folders.rb
@@ -7,18 +7,16 @@ require 'fileutils'
 class Folders
 
   def self.set( socket )
-    Thread.new{
-        data = socket.read
-        p data
-        json_data = JSON.parse( data )
+      data = socket.read
+      p data
+      json_data = JSON.parse( data )
 
-        fl = FoldersList.find_or_initialize_by( guid: json_data['guid'] ){ |f_list|
-          f_list.url_hash = SecureRandom.hex(16)
-        }
-        fl.folders = json_data['folders']
-        fl.save!
-        p fl
-    }
+      fl = FoldersList.find_or_initialize_by( guid: json_data['guid'] ){ |f_list|
+        f_list.url_hash = SecureRandom.hex(16)
+      }
+      fl.folders = json_data['folders']
+      fl.save!
+      p fl
   end
 
 end
--- server/tasks_dispatcher.rb
+++ server/tasks_dispatcher.rb
@@ -3,12 +3,14 @@ require_relative 'folders'
 class TasksDispatcher
 
   def self.process_connection( socket )
-    command, _ =  socket.recvfrom( 3 )
+    Thread.new{
+      command, _ =  socket.recvfrom( 3 )
 
-    case when command
-           'SFO'
-           Folders.set( socket )
-    end
+      case when command
+             'SFO'
+             Folders.set( socket )
+      end
+    }
   end
 
 end
